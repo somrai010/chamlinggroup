421732f27ada6d86c63e45af213a065055fc7582
Ayush Thapa <thapaaayush115@gmail.com>
2023-03-15 21:06:02 +0545

    fix: fix cover photo3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,14 @@ export const Header = (props) => {
   return (
     <div>
       <div className="relative object-cover w-full h-[90vh]">
-        <Image fill src="/chamlingcover.jpg" alt="header image" />
-        <div className="absolute w-full h-auto md:top-[45vh] top-[50vh] text-center">
+        <Image fill src="/cover3.jpg" alt="header image" />
+        <div className="absolute w-full h-auto md:top-[55vh] top-[50vh] text-center">
           <div>
-            <h2 className=" font-marcellus md:text-[40px] text-[25px] text-text-black-default ">Chamling Group 2023 | Achievements</h2>
+            <h2 className=" font-marcellus md:text-[40px] text-[25px] text-white-default ">Chamling Group 2023 | Achievements</h2>
           </div>
           <div className="mt-5 ">
             <Fade direction="up">
-            <Button variant="outlined"  endIcon={<ArrowForwardIosIcon/>} sx={{color:'#0F0C28',border:'2px solid red',borderRadius:'20px',fontSize:'27px',px:'60px'}}>View</Button>
+            <Button variant="outlined"  endIcon={<ArrowForwardIosIcon/>} sx={{color:'white',border:'2px solid white',borderRadius:'20px',fontSize:'27px',px:'60px'}}>View</Button>
             </Fade>
           </div>
         </div>
